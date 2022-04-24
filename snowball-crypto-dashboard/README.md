bdf987518df40160b54a80605c1f41548b5adf49
arisanator9900 <96804792+arisanator9900@users.noreply.github.com>
2022-04-24 01:20:57 -0400

    snowball crypto dashboard github repository

---
+++ snowball-crypto-dashboard/README.md
@@ -0,0 +1,9 @@
+This is a crypto dashboard, showing the current prices of crypto in a page format.
+this is also searchable using the searchbar to find any cryptocurrency in the top 100
+
+this application uses React, React Router, Chart.js and Coingecko API
+This application also uses MUI for direct styling.
+
+*This application was built using an old version of react because of the current issues with react routing
+
+This project dives into the use of API's while implementing them directly into a web based application.
---
+++ snowball-crypto-dashboard/src/components/SelectButton.js
@@ -0,0 +1,33 @@
+import { makeStyles } from "@material-ui/core";
+
+const SelectButton = ({ children, selected, onClick }) => {
+  const useStyles = makeStyles({
+    selectbutton: {
+      border: "1px solid #FF7217",
+      borderRadius: 5,
+      padding: 10,
+      paddingLeft: 20,
+      paddingRight: 20,
+      fontFamily: "Montserrat",
+      cursor: "pointer",
+      backgroundColor: selected ? "#FF7217" : "#FF7217",
+      color: selected ? "#fff" : "#fff",
+      fontWeight: selected ? 700 : 500,
+      "&:hover": {
+        backgroundColor: "#FF7217",
+        color: "black",
+      },
+      width: "22%",
+    },
+  });
+
+  const classes = useStyles();
+
+  return (
+    <span onClick={onClick} className={classes.selectbutton}>
+      {children}
+    </span>
+  );
+};
+
+export default SelectButton;
